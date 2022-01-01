0ca19e21d46306d1d4e0e7a80a4ab466414cb814
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-01-01 01:46:36 +0100

    Loop until eternity

--- src/main/java/de/ialistannen/shipit/Main.java
+++ src/main/java/de/ialistannen/shipit/Main.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,11 +23,16 @@ public class Main {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
-    if (args.length != 1) {
-      System.err.println("Usage: <this program> <discord webhook url>");
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    if (args.length < 1 || args.length > 2) {
+      System.err.println("Usage: <this program> <discord webhook url> [check frequency in seconds]");
       System.exit(1);
     }
+    int checkFrequencySeconds = (int) TimeUnit.HOURS.toSeconds(6);
+    if (args.length == 2) {
+      checkFrequencySeconds = Integer.parseInt(args[1]);
+    }
+
     HttpClient httpClient = HttpClient.newBuilder().build();
 
     DefaultDockerClientConfig.Builder config = DefaultDockerClientConfig.createDefaultConfigBuilder();
@@ -36,15 +43,32 @@ public class Main {
     ImageUpdateChecker imageUpdateChecker = new ImageUpdateChecker(dockerClient, registryClient);
     ContainerUpdateChecker containerUpdateChecker = new ContainerUpdateChecker(dockerClient, imageUpdateChecker);
 
-    DiscordNotifier notifier = new DiscordNotifier(httpClient, new URI(args[0])
-    );
+    DiscordNotifier notifier = new DiscordNotifier(httpClient, new URI(args[0]));
 
-    try {
-      List<ShipItContainerUpdate> updates = containerUpdateChecker.check();
-      notifier.notify(updates);
-    } catch (Exception e) {
-      LOGGER.error("Failed to check for updates", e);
-      notifier.notify(e);
+    //noinspection InfiniteLoopStatement
+    while (true) {
+      try {
+        LOGGER.info("Checking for updates...");
+        List<ShipItContainerUpdate> updates = containerUpdateChecker.check();
+        notifier.notify(updates);
+      } catch (Exception e) {
+        LOGGER.error("Failed to check for updates", e);
+        notifier.notify(e);
+      }
+      Instant nextWakeup = Instant.now().plusSeconds(checkFrequencySeconds);
+      while (Instant.now().isBefore(nextWakeup)) {
+        LOGGER.debug(
+          "Sleeping until {} ({} seconds)",
+          nextWakeup,
+          nextWakeup.getEpochSecond() - Instant.now().getEpochSecond()
+        );
+        try {
+          //noinspection BusyWait
+          Thread.sleep(checkFrequencySeconds / 4 * 1000L);
+        } catch (InterruptedException e) {
+          LOGGER.info("Interrupted while sleeping", e);
+        }
+      }
     }
   }
 }
